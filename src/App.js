8c52c95f6beaab27581946fa9861bc2ff119b67b
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2020-01-19 01:56:54 -0300

    correções de bootstrap responsive login

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const loading = () => <div className="animated fadeIn pt-3 text-center">Loading.
 const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout1'));
 
 // Pages
-const Login = React.lazy(() => import('./views/Pages/Login'));
-const Register = React.lazy(() => import('./views/Pages/Register'));
+const Login = React.lazy(() => import('./views/Pages/Login/Login.js'));
+const Register = React.lazy(() => import('./views/Pages/Register/Register.js'));
 //const Home = React.lazy(() => import('./views/Pages/Home/home'));
 const Page404 = React.lazy(() => import('./views/Pages/Page404'));
 const Page500 = React.lazy(() => import('./views/Pages/Page500'));
@@ -23,7 +23,7 @@ class App extends Component {
       <BrowserRouter>
           <React.Suspense fallback={loading()}>
             <Switch>
-              <Route path="/" name="Login Page" render={props => <Login {...props}/>} />
+              <Route path="/login" name="Login Page" render={props => <Login {...props}/>} />
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
