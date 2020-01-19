1c7b143c8c8736b8c70346a73f9a632a2c29bf40
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2020-01-19 02:30:23 -0300

    fix bug renderização container

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
               <Route exact path="/home" name="List" render={props => <DefaultLayout {...props}/>} />
-              {/*<Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />*/}
+              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
               
             </Switch>
           </React.Suspense>
